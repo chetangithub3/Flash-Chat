d54a5437c88f50c546c4922e081bcfed994273d0
Chetan <chetan.getsmail@gmail.com>
2022-04-24 01:29:25 -0400

    Connected Firestore

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,99 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
+    
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = [
+        
+    ]
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        navigationItem.hidesBackButton = true
+        navigationItem.title = K.appName
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                                                                      K.FStore.dateField: Date.init(timeIntervalSince1970: 1.0)]) { error in
+                if let e = error{
+                    print(e.localizedDescription)
+                }else{
+                    print("Saved successfully")
+                }
+                self.loadTableView()
+            }
+        }
+        
     }
     
-
+    func loadTableView(){
+        self.messages = []
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                if let querySnapshotDocuments = querySnapshot?.documents{
+                    for document in querySnapshotDocuments {
+                         let data = document.data()
+                        
+                        
+                        if let message = data[K.FStore.bodyField] as? String,
+                            let sender = data[K.FStore.senderField] as? String
+                        {
+                            
+                            let newMessage = Message(sender: sender, body: message)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+    do {
+      try firebaseAuth.signOut()
+        navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+      print("Error signing out: %@", signOutError)
+    }
+      
+    }
+    
+}
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Chetan Dhowlaghar on 4/20/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Message{
+    var sender: String
+    var body: String
+}
